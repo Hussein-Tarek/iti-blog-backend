583dac1b860111cff63628563d6ecf5046d39986
Hussein-tarek <htarek47@gmail.com>
2023-04-01 15:39:12 +0200

    edit post

--- schema/post/postSchema.js
+++ schema/post/postSchema.js
@@ -11,7 +11,7 @@ const postSchema = new Schema(
     content: {
       type: String,
     },
-    photo: Array,
+    photo: String,
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
