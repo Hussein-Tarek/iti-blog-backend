a5ac7b26fd770cb8f780bb4ba59e0a5ef4e5ee2f
Hussein-tarek <htarek47@gmail.com>
2023-04-11 01:33:02 +0200

    profile cover

--- controllers/auth/sign-up.js
+++ controllers/auth/sign-up.js
@@ -15,10 +15,11 @@ exports.signUp = async (req, res, next) => {
         400
       );
     }
-    console.log("out photo", req.file)
-    if (req.file?.photo) {
-      console.log("photo", req.file.photo)
-      user.photo = await uploadCloudBB(req.file.photo);
+    if (req.files?.photo) {
+      user.photo = await uploadCloudBB(...req.files.photo);
+    }
+    if (req.files?.cover_photo) {
+      user.cover_photo = await uploadCloudBB(...req.files.cover_photo);
     }
     // hash password
     const pass = await hashPassword(password);
@@ -44,3 +45,51 @@ exports.signUp = async (req, res, next) => {
     next(err);
   }
 };
+
+
+
+
+
+
+
+
+
+
+// const { userModel } = require("../../models/index");
+// const { errorHandler, successHandler } = require("../../utils/responseHandler");
+// const { hashPassword, signUserToken } = require("./auth");
+
+// exports.signUp = async (req, res, next) => {
+//   try {
+//     const { username, email, password } = req.body;
+
+//     // check if user exist
+//     const checkedUser = await userModel.findOne({ email });
+//     if (checkedUser) {
+//       throw errorHandler(
+//         "email is already exist try different email or sign in",
+//         400
+//       );
+//     }
+//     // hash password
+//     const pass = await hashPassword(password);
+
+//     // create new user
+
+//     await userModel.create({ ...req.body, password: pass });
+
+//     //get the user
+//     const newUser = await userModel.findOne({ email });
+
+//     //add access token
+//     const access_token = await signUserToken(newUser.id);
+
+//     // handle response
+//     successHandler(res, {
+//       access_token,
+//       user: newUser,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
--- middlewares/imgbb/imgbb.js
+++ middlewares/imgbb/imgbb.js
@@ -1,3 +1,41 @@
+// const fs = require("fs");
+// const imgbbUploader = require("imgbb-uploader");
+// const dotenv = require("dotenv");
+// const { errorHandler } = require("../../utils/responseHandler");
+// dotenv.config();
+
+// exports.uploadCloudBB = async (file) => {
+//   console.log("uploading img to imgbb");
+//   try {
+//     const result = await imgbbUploader(process.env.IMGBB_KEY, file);
+//     if (result) {
+//       console.log(result);
+//       fs.unlink(file, function (err) {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//       return { url: result.url, public_id: result.id };
+//       // return result.url;
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     throw errorHandler("failed to upload images", 500);
+//   }
+// };
+
+// exports.cloudUploadImages = (arr) => {
+//   return Promise.all(arr.map(async (item) => await this.uploadCloud(item)));
+// };
+
+
+
+
+
+
+
+
+
 const fs = require("fs");
 const imgbbUploader = require("imgbb-uploader");
 const dotenv = require("dotenv");
@@ -16,7 +54,6 @@ exports.uploadCloudBB = async (file) => {
         }
       });
       return { url: result.url, public_id: result.id };
-      // return result.url;
     }
   } catch (err) {
     console.log(err);
